269fd6cb010cfca201b9a9aaa69a185e1b66776f
Hugo343 <gogabriel2003@gmaill.com>
2025-07-20 02:55:32 +0700

    perbaiki routes

--- routes/web.php
+++ routes/web.php
@@ -57,20 +57,8 @@ Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.d
 ; 
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 // Health check route
 Route::get('/health', function () {
